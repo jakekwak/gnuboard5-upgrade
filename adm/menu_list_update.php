baf114cb7a59323cb19debb45a09530c3c50c381
Jake Kwak <jakekwak@gmail.com>
2021-05-25 17:12:20 -0500

    me_parent_id에 값 넣기

--- adm/menu_list_update.php
+++ adm/menu_list_update.php
@@ -15,7 +15,9 @@ sql_query($sql);
 
 $group_code = null;
 $primary_code = null;
+$menu_id = 0;
 $count = count($_POST['code']);
+fb($count);
 
 for ($i=0; $i<$count; $i++)
 {
@@ -28,6 +30,7 @@ for ($i=0; $i<$count; $i++)
     $_POST['me_link'][$i] = is_array($_POST['me_link']) ? clean_xss_tags(clean_xss_attributes(preg_replace('/[ ]{2,}|[\t]/', '', $_POST['me_link'][$i]), 1)) : '';
 
     $code    = is_array($_POST['code']) ? strip_tags($_POST['code'][$i]) : '';
+    fb($code);
     $me_name = is_array($_POST['me_name']) ? strip_tags($_POST['me_name'][$i]) : '';
     $me_link = (preg_match('/^javascript/i', $_POST['me_link'][$i]) || preg_match('/script:/i', $_POST['me_link'][$i])) ? G5_URL : strip_tags(clean_xss_attributes($_POST['me_link'][$i]));
     
@@ -35,26 +38,39 @@ for ($i=0; $i<$count; $i++)
         continue;
 
     $sub_code = '';
+    $me_parent_id = 0;
     if($group_code == $code) {
+
         $sql = " select MAX(SUBSTRING(me_code,3,2)) as max_me_code
                     from {$g5['menu_table']}
                     where SUBSTRING(me_code,1,2) = '$primary_code' ";
         $row = sql_fetch($sql);
+        fb($row);
 
         $sub_code = base_convert($row['max_me_code'], 36, 10);
+        fb($me_code);
         $sub_code += 36;
+        fb($me_code);
         $sub_code = base_convert($sub_code, 10, 36);
+        fb($me_code);
 
         $me_code = $primary_code.$sub_code;
+        fb($me_code);
+        $me_parent_id = $menu_id;
     } else {
+
         $sql = " select MAX(SUBSTRING(me_code,1,2)) as max_me_code
                     from {$g5['menu_table']}
                     where LENGTH(me_code) = '2' ";
         $row = sql_fetch($sql);
+        fb($row);
 
         $me_code = base_convert($row['max_me_code'], 36, 10);
+        fb($me_code);
         $me_code += 36;
+        fb($me_code);
         $me_code = base_convert($me_code, 10, 36);
+        fb($me_code);
 
         $group_code = $code;
         $primary_code = $me_code;
@@ -63,13 +79,16 @@ for ($i=0; $i<$count; $i++)
     // 메뉴 등록
     $sql = " insert into {$g5['menu_table']}
                 set me_code         = '".$me_code."',
+                    me_parent_id    = '".$me_parent_id."',
                     me_name         = '".$me_name."',
                     me_link         = '".$me_link."',
                     me_target       = '".sql_real_escape_string(strip_tags($_POST['me_target'][$i]))."',
                     me_order        = '".sql_real_escape_string(strip_tags($_POST['me_order'][$i]))."',
                     me_use          = '".sql_real_escape_string(strip_tags($_POST['me_use'][$i]))."',
                     me_mobile_use   = '".sql_real_escape_string(strip_tags($_POST['me_mobile_use'][$i]))."' ";
-    sql_query($sql);
+    $row=sql_query($sql);
+    if(!$me_parent_id)
+        $menu_id = $g5['connect_db']->insert_id;	   
 }
 
 run_event('admin_menu_list_update');
